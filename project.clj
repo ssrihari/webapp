9d898a44d8b1c1eeb39ad87d2da26ef881a59219
Srihari Sriraman <srihari@nilenso.com>
2019-03-04 09:35:18 +0530

    init webapp

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject webapp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [bidi "2.1.5"]
+                 [honeysql "0.9.4"]
+                 [hikari-cp "2.7.0"]
+                 [nilenso/honeysql-postgres "0.2.5"]
+                 [ring "1.7.1"]
+                 [cheshire "5.8.1"]])
